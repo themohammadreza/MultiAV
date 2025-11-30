3dde407abba809527046b26d15b4a83c0ea38668
themohammadreza <themohammadreza2006@gmail.com>
2025-11-30 05:48:47 +0000

    Renamebd Database from multiav to multiav_db

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 
-SQLALCHEMY_DATABASE_URL = "postgresql://multiav_user:mohammad@localhost:5432/multiav"
+SQLALCHEMY_DATABASE_URL = "postgresql://multiav_user:mohammad@localhost:5432/multiav_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
